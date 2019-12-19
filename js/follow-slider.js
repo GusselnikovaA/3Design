1f790adb613ceac58606d4a7ef55e44f53ab0bca
GusselnikovaA <annanac1993@gmail.com>
2019-12-19 15:49:14 +0300

    Блок с разделом инстаграм

---
+++ js/follow-slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  // СЛАЙДЕР В БЛОКЕ FOLLOW
+  $('.follow-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    speed: 2000
+  });
+});
