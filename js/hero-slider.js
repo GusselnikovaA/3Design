5fbae521ae04365454dbc565b7f4b5d6ea78d4c4
GusselnikovaA <annanac1993@gmail.com>
2020-01-14 11:54:11 +0300

    cssEase в hero-slider

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -1,13 +1,15 @@
-// $(document).ready(function () {
+$(document).ready(function () {
   // СЛАЙДЕР В БЛОКЕ HERO 
-//   $('.hero-slider').slick({
-//     infinite: false,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 1000,
-//     arrows: false,
-//     fade: true,
-//     // speed: 1000, 
-//   });
-// });
+  $('.hero-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 300,
+    arrows: false,
+    fade: true,
+    speed: 300,
+    cssEase: 'easeInOutBack',
+    infinite: false
+  });
+});
+
