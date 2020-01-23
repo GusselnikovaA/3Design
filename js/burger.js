dd4d9753da2aa0132463c5cf383f44c7ee38b662
GusselnikovaA <annanac1993@gmail.com>
2020-01-23 14:40:46 +0300

    Анимация бургер меню на CSS

--- js/burger.js
+++ js/burger.js
@@ -1,16 +1,20 @@
   
 $(document).ready(function(){
-  var menuAlert = $('.burger_alert');
+  var burgerAlert = $('.burger_alert');
   var modalMenu = $('#menu_mobile');
   var closeMenu = $('#burger_close');
+  var burger = $('.header-burger__line');
 
-  menuAlert.on('click', function(){
-    modalMenu.addClass('menu-mobile_active');
+  burgerAlert.on('click', function(){
+    modalMenu.toggleClass('menu-mobile_active');
+    burger.toggleClass('header-burger__line_active');
   });
+
   $('.menu-list__item').on('click', function(){
     modalMenu.removeClass('menu-mobile_active');
   });
-  closeMenu.on('click', function(){
-    modalMenu.removeClass('menu-mobile_active');
-  });
+  // closeMenu.on('click', function(){
+  //   modalMenu.removeClass('menu-mobile_active');
+  //   burger.removeClass('header-burger__line_active');
+  // });
 });
