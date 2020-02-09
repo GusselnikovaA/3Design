797a965456900d4447c3ffc32ad43fa1c3495df7
Anna <annanac1993@gmail.com>
2020-02-10 00:47:33 +0300

    Устранение проблемы с переходом по ссылке (удаление event.PreventDefault и переназначение галереи на project-portfolio__cards

--- src/js/arrow.js
+++ src/js/arrow.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $(".header-list,.arrow").on("click","a", function (event) {
-    event.preventDefault();
     var id  = $(this).attr('href'),
         top = $(id).offset().top;
     $('body,html').animate({scrollTop: top}, 1500);
